383fe3016bb1f6489847fb6d902c72e0458eb1c4
Mochammad Agus Yahya <mochammadagusyahya@gmail.com>
2022-08-23 17:45:43 +0700

    update personal website

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -78,6 +78,15 @@ WSGI_APPLICATION = 'personal_website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
+"""
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
+"""
 DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
